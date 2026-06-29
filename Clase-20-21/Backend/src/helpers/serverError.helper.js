dac01cd5c12b6d2226d8214ed3df08f4723bdd62
class-sketch-glitch <class-sketch-glitch@users.noreply.github.com>
2026-06-29 14:54:53 -0300

    fix: index limpio y export default app

--- Clase-20-21/Backend/src/helpers/serverError.helper.js
+++
@@ -1,7 +0,0 @@
-class ServerError  extends Error {
-    constructor(message, status){
-        super(message)
-        this.status = status
-    }
-}
-export default ServerError
--- Clase-20-21/Frontend/config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config(); // Carga las variables del archivo .env
-
-export const ENVIRONMENT = {
-    URL_FRONTEND: process.env.URL_FRONTEND ,
-   
-};
